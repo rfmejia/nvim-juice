b91fec60d477ca4a989295f8e06a5cf2c48169c9
Akie Mejia <rfmejia@pm.me>
2024-06-03 04:39:30 -0700

    Change plugin groupings; Add `neogit`

--- fnl/juice/plugins.fnl
+++ fnl/juice/plugins.fnl
@@ -1,7 +1,6 @@
 (local {: autoload} (require :nfnl.module))
 (local {: concat} (autoload :nfnl.core))
-(local {: auto-setup : set-opts} (autoload :juice.util))
-(local {: nmap} (autoload :juice.util))
+(local {: auto-setup : nmap : set-opts} (autoload :juice.util))
 
 (local core [{1 :Olical/nfnl :ft :fennel}
              {1 :projekt0n/github-nvim-theme
@@ -31,45 +30,6 @@
                                       :indent {:enable true}}]
                           (ts.setup config)))}])
 
-(local ui-tools
-       [{1 :stevearc/oil.nvim
-         :cmd :Oil
-         :config {:default_file_explorer true :delete_to_trash true}}
-        {1 :nvim-telescope/telescope.nvim
-         :tag :0.1.6
-         :dependencies [:nvim-lua/plenary.nvim]
-         :config (fn []
-                   (let [telescope (autoload :telescope)
-                         actions (autoload :telescope.actions)
-                         config {:defaults {:border false
-                                            :layout_config {:prompt_position :bottom
-                                                            :height 0.6}
-                                            :layout_strategy :bottom_pane
-                                            :mappings {:i {:<esc> actions.close
-                                                           :<C-u> false}}
-                                            :path_display {1 :truncate}
-                                            :preview false
-                                            :prompt_prefix "/"}}]
-                     (telescope.setup config)))}
-        {1 :lewis6991/gitsigns.nvim
-         :event [:BufReadPre :BufNewFile]
-         :config true}
-        {1 :mbbill/undotree
-         :cmd :UndotreeToggle
-         :config (fn []
-                   (set vim.g.undotree_WindowLayout 4)
-                   (set vim.g.undotree_SetFocusWhenToggle 1))}])
-
-(local text-tools
-       [{1 :kylechui/nvim-surround :keys [:cs :ds :ys] :config true}])
-
-(local dev-tools
-       [{1 :neovim/nvim-lspconfig :config #(auto-setup :juice.lsp)}
-        {1 :scalameta/nvim-metals
-         :cmd :MetalsInit
-         :dependencies [:nvim-lua/plenary.nvim]}
-        {1 :Olical/conjure :ft [:clojure :fennel :lisp :scheme]}])
-
 (local database-tools
        [{1 :kristijanhusak/vim-dadbod-ui
          :cmd [:DBUI :DBUIToggle]
@@ -82,6 +42,51 @@
                          :lazy true
                          :ft [:sql :mysql]}]}])
 
+(local dev-tools
+       [{1 :neovim/nvim-lspconfig :config #(auto-setup :juice.lsp)}
+        {1 :scalameta/nvim-metals
+         :cmd :MetalsInit
+         :dependencies [:nvim-lua/plenary.nvim]}
+        {1 :Olical/conjure :ft [:clojure :fennel :lisp :scheme]}])
+
+(local editing-tools
+       [{1 :kylechui/nvim-surround :keys [:cs :ds :ys] :config true}
+        {1 :mbbill/undotree
+         :cmd :UndotreeToggle
+         :config (fn []
+                   (set vim.g.undotree_WindowLayout 4)
+                   (set vim.g.undotree_SetFocusWhenToggle 1))}])
+
+(local file-tools [{1 :stevearc/oil.nvim
+                    :cmd :Oil
+                    :config {:default_file_explorer true :delete_to_trash true}}
+                   {1 :nvim-telescope/telescope.nvim
+                    :tag :0.1.6
+                    :dependencies [:nvim-lua/plenary.nvim]
+                    :config (fn []
+                              (let [telescope (autoload :telescope)
+                                    actions (autoload :telescope.actions)
+                                    config {:defaults {:border false
+                                                       :layout_config {:prompt_position :bottom
+                                                                       :height 0.4}
+                                                       :layout_strategy :bottom_pane
+                                                       :mappings {:i {:<esc> actions.close
+                                                                      :<C-u> false}}
+                                                       :path_display {1 :truncate}
+                                                       :preview false
+                                                       :prompt_prefix "/"
+                                                       :prompt_title :test}}]
+                                (telescope.setup config)))}])
+
+(local git-tools [{1 :lewis6991/gitsigns.nvim
+                   :event [:BufReadPre :BufNewFile]
+                   :config true}
+                  {1 :NeogitOrg/neogit
+                   :dependencies [{1 :nvim-lua/plenary.nvim}
+                                  {1 :sindrets/diffview.nvim}
+                                  {1 :nvim-telescope/telescope.nvim}]
+                   :config true}])
+
 (local vim-fu [{1 :m4xshen/hardtime.nvim
                 :dependencies [{1 :MunifTanjim/nui.nvim}
                                {1 :nvim-lua/plenary.nvim}]
@@ -89,8 +94,8 @@
 
 (fn setup []
   (let [lazy (autoload :lazy)
-        plugins (concat core ui-tools text-tools dev-tools database-tools
-                        vim-fu)
+        plugins (concat core database-tools dev-tools editing-tools file-tools
+                        git-tools vim-fu)
         opts {:ui {:border :rounded}
               :performance {:rtp {:disabled_plugins [:rplugin
                                                      :tohtml
--- lua/juice/plugins.lua
+++ lua/juice/plugins.lua
@@ -5,51 +5,52 @@ local _local_2_ = autoload("nfnl.core")
 local concat = _local_2_["concat"]
 local _local_3_ = autoload("juice.util")
 local auto_setup = _local_3_["auto-setup"]
+local nmap = _local_3_["nmap"]
 local set_opts = _local_3_["set-opts"]
-local _local_4_ = autoload("juice.util")
-local nmap = _local_4_["nmap"]
 local core
-local function _5_()
+local function _4_()
   return auto_setup("juice.colors")
 end
-local function _6_()
+local function _5_()
   local ts = autoload("nvim-treesitter.configs")
   local languages = {"bash", "fennel", "gitcommit", "go", "hocon", "java", "json", "lua", "markdown", "scala", "sql", "vimdoc", "yaml"}
   local config = {ensure_installed = languages, highlight = {enable = true}, indent = {enable = true}}
   return ts.setup(config)
 end
-core = {{"Olical/nfnl", ft = "fennel"}, {"projekt0n/github-nvim-theme", priority = 1000, config = _5_, lazy = false}, {"nvim-treesitter/nvim-treesitter", event = {"BufReadPre", "BufNewFile"}, build = ":TSUpdate", config = _6_}}
-local ui_tools
-local function _7_()
-  local telescope = autoload("telescope")
-  local actions = autoload("telescope.actions")
-  local config = {defaults = {layout_config = {prompt_position = "bottom", height = 0.6}, layout_strategy = "bottom_pane", mappings = {i = {["<esc>"] = actions.close, ["<C-u>"] = false}}, path_display = {"truncate"}, prompt_prefix = "/", border = false, preview = false}}
-  return telescope.setup(config)
+core = {{"Olical/nfnl", ft = "fennel"}, {"projekt0n/github-nvim-theme", priority = 1000, config = _4_, lazy = false}, {"nvim-treesitter/nvim-treesitter", event = {"BufReadPre", "BufNewFile"}, build = ":TSUpdate", config = _5_}}
+local database_tools
+local function _6_()
+  local function _7_()
+    return set_opts({commentstring = "-- %s", omnifunc = "vim_dadbod_completion#omni"})
+  end
+  return vim.api.nvim_create_autocmd("FileType", {pattern = {"sql", "mysql"}, callback = _7_})
 end
+database_tools = {{"kristijanhusak/vim-dadbod-ui", cmd = {"DBUI", "DBUIToggle"}, config = _6_, dependencies = {{"tpope/vim-dadbod", lazy = true}, {"kristijanhusak/vim-dadbod-completion", lazy = true, ft = {"sql", "mysql"}}}}}
+local dev_tools
 local function _8_()
+  return auto_setup("juice.lsp")
+end
+dev_tools = {{"neovim/nvim-lspconfig", config = _8_}, {"scalameta/nvim-metals", cmd = "MetalsInit", dependencies = {"nvim-lua/plenary.nvim"}}, {"Olical/conjure", ft = {"clojure", "fennel", "lisp", "scheme"}}}
+local editing_tools
+local function _9_()
   vim.g.undotree_WindowLayout = 4
   vim.g.undotree_SetFocusWhenToggle = 1
   return nil
 end
-ui_tools = {{"stevearc/oil.nvim", cmd = "Oil", config = {default_file_explorer = true, delete_to_trash = true}}, {"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = {"nvim-lua/plenary.nvim"}, config = _7_}, {"lewis6991/gitsigns.nvim", event = {"BufReadPre", "BufNewFile"}, config = true}, {"mbbill/undotree", cmd = "UndotreeToggle", config = _8_}}
-local text_tools = {{"kylechui/nvim-surround", keys = {"cs", "ds", "ys"}, config = true}}
-local dev_tools
-local function _9_()
-  return auto_setup("juice.lsp")
-end
-dev_tools = {{"neovim/nvim-lspconfig", config = _9_}, {"scalameta/nvim-metals", cmd = "MetalsInit", dependencies = {"nvim-lua/plenary.nvim"}}, {"Olical/conjure", ft = {"clojure", "fennel", "lisp", "scheme"}}}
-local database_tools
+editing_tools = {{"kylechui/nvim-surround", keys = {"cs", "ds", "ys"}, config = true}, {"mbbill/undotree", cmd = "UndotreeToggle", config = _9_}}
+local file_tools
 local function _10_()
-  local function _11_()
-    return set_opts({commentstring = "-- %s", omnifunc = "vim_dadbod_completion#omni"})
-  end
-  return vim.api.nvim_create_autocmd("FileType", {pattern = {"sql", "mysql"}, callback = _11_})
+  local telescope = autoload("telescope")
+  local actions = autoload("telescope.actions")
+  local config = {defaults = {layout_config = {prompt_position = "bottom", height = 0.4}, layout_strategy = "bottom_pane", mappings = {i = {["<esc>"] = actions.close, ["<C-u>"] = false}}, path_display = {"truncate"}, prompt_prefix = "/", prompt_title = "test", border = false, preview = false}}
+  return telescope.setup(config)
 end
-database_tools = {{"kristijanhusak/vim-dadbod-ui", cmd = {"DBUI", "DBUIToggle"}, config = _10_, dependencies = {{"tpope/vim-dadbod", lazy = true}, {"kristijanhusak/vim-dadbod-completion", lazy = true, ft = {"sql", "mysql"}}}}}
+file_tools = {{"stevearc/oil.nvim", cmd = "Oil", config = {default_file_explorer = true, delete_to_trash = true}}, {"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = {"nvim-lua/plenary.nvim"}, config = _10_}}
+local git_tools = {{"lewis6991/gitsigns.nvim", event = {"BufReadPre", "BufNewFile"}, config = true}, {"NeogitOrg/neogit", dependencies = {{"nvim-lua/plenary.nvim"}, {"sindrets/diffview.nvim"}, {"nvim-telescope/telescope.nvim"}}, config = true}}
 local vim_fu = {{"m4xshen/hardtime.nvim", dependencies = {{"MunifTanjim/nui.nvim"}, {"nvim-lua/plenary.nvim"}}, config = true}}
 local function setup()
   local lazy = autoload("lazy")
-  local plugins = concat(core, ui_tools, text_tools, dev_tools, database_tools, vim_fu)
+  local plugins = concat(core, database_tools, dev_tools, editing_tools, file_tools, git_tools, vim_fu)
   local opts = {ui = {border = "rounded"}, performance = {rtp = {disabled_plugins = {"rplugin", "tohtml", "tutor", "vimball"}}}}
   return lazy.setup(plugins, opts)
 end
