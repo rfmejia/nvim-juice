34ac5f725ff517970a28147c68618497239c73b5
Akie Mejia <rfmejia@pm.me>
2024-09-23 16:29:45 -0700

    Add more journal-specific keymaps

--- after/ftplugin/asciidoc.lua
+++ after/ftplugin/asciidoc.lua
@@ -6,18 +6,63 @@ local executable_3f = _local_2_["executable?"]
 local set_opts = _local_2_["set-opts"]
 local util = autoload("juice.util")
 util["set-opts"]({shiftwidth = 2, tabstop = 2, textwidth = 80, wrap = true, spell = true, spelllang = "en_us"})
-util["set-keys"]({{"n", "<localleader>d", ":r!date '+\\%a, \\%d \\%b \\%Y' | xargs -0 printf '\\n== \\%s\\n\\n'<cr>k", {desc = "insert current date as an h2 header", buffer = vim.api.nvim_get_current_buf(), silent = true}}, {"n", "<localleader>t", ":r!date '+\\%H:\\%M' | xargs -0 printf '=== \\%s ' | tr -d '\\n'<cr>A", {desc = "insert current time as an h3 header", buffer = vim.api.nvim_get_current_buf(), silent = true}}})
+local function insert_lines(text)
+  local buf = vim.api.nvim_get_current_buf()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local _row = (row - 1)
+  return vim.api.nvim_buf_set_lines(buf, _row, (_row + 1), false, text)
+end
+local function insert_week()
+  local function find_day(dir, day, new_time)
+    _G.assert((nil ~= new_time), "Missing argument new-time on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:20")
+    _G.assert((nil ~= day), "Missing argument day on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:20")
+    _G.assert((nil ~= dir), "Missing argument dir on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:20")
+    local new_day = vim.fn.strftime("%a", new_time)
+    local secs_in_a_day = (60 * 60 * 24)
+    if (day == new_day) then
+      return vim.fn.strftime("%b %d", new_time)
+    else
+      local function _3_()
+        if (dir == "fwd") then
+          return (new_time + secs_in_a_day)
+        else
+          return (new_time - secs_in_a_day)
+        end
+      end
+      return find_day(dir, day, _3_())
+    end
+  end
+  local week_num = vim.fn.strftime("%U")
+  local week_start = find_day("back", "Mon", vim.fn.localtime())
+  local week_end = find_day("fwd", "Sun", vim.fn.localtime())
+  local lines = {"'''", ("= Week " .. week_num .. " (" .. week_start .. " to " .. week_end .. ")"), ""}
+  return insert_lines(lines)
+end
+local function insert_day()
+  local curr_day = vim.fn.strftime("%a, %d %b %Y")
+  local lines = {("== " .. curr_day), ""}
+  return insert_lines(lines)
+end
+local function insert_time()
+  local curr_time = vim.fn.strftime("%H:%M")
+  local lines = {("=== " .. curr_time), ""}
+  return insert_lines(lines)
+end
+local function insert_task()
+  return insert_lines({"* [ ] "})
+end
+--[[ (insert-week) ]]
 local function preview_in_browser(_in, out, browser_cmd)
-  _G.assert((nil ~= browser_cmd), "Missing argument browser-cmd on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:25")
-  _G.assert((nil ~= out), "Missing argument out on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:25")
-  _G.assert((nil ~= _in), "Missing argument in on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:25")
+  _G.assert((nil ~= browser_cmd), "Missing argument browser-cmd on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:49")
+  _G.assert((nil ~= out), "Missing argument out on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:49")
+  _G.assert((nil ~= _in), "Missing argument in on /home/rfmejia/.config/nvim/fnl/after/ftplugin/asciidoc.fnl:49")
   if util["executable?"]("asciidoctor") then
-    local _3_, _4_ = vim.fn.system({"asciidoctor", "-o", out, _in})
-    if (nil ~= _3_) then
-      local ok = _3_
+    local _5_, _6_ = vim.fn.system({"asciidoctor", "-o", out, _in})
+    if (nil ~= _5_) then
+      local ok = _5_
       return vim.fn.system({browser_cmd, out})
-    elseif ((_3_ == nil) and (nil ~= _4_)) then
-      local err_msg = _4_
+    elseif ((_5_ == nil) and (nil ~= _6_)) then
+      local err_msg = _6_
       return error(("Could not run asciidoctor: " .. err_msg))
     else
       return nil
@@ -29,10 +74,10 @@ end
 if vim.env.BROWSER then
   local _in = vim.fn.expand("%:p")
   local out = "/tmp/preview.html"
-  local function _7_()
+  local function _9_()
     return preview_in_browser(_in, out, vim.env.BROWSER)
   end
-  return vim.keymap.set("n", "<localleader>p", _7_, {desc = "convert to HTML and show preview in browser", buffer = vim.api.nvim_get_current_buf()})
+  vim.keymap.set("n", "<localleader>p", _9_, {desc = "convert to HTML and show preview in browser", buffer = vim.api.nvim_get_current_buf()})
 else
-  return nil
 end
+return util["set-keys"]({{"n", "<localleader>w", insert_week, {desc = "insert current week as an h2 header", buffer = vim.api.nvim_get_current_buf(), silent = true}}, {"n", "<localleader>d", ":r!date '+\\%a, \\%d \\%b \\%Y' | xargs -0 printf '\\n== \\%s\\n\\n'<cr>k", {desc = "insert current date as an h2 header", buffer = vim.api.nvim_get_current_buf(), silent = true}}, {"n", "<localleader>t", insert_time, {desc = "insert current time as an h3 header", buffer = vim.api.nvim_get_current_buf(), silent = true}}, {"n", "<localleader>x", insert_task, {desc = "insert asciidoc checkbox", buffer = vim.api.nvim_get_current_buf(), silent = true}}})
--- fnl/after/ftplugin/asciidoc.fnl
+++ fnl/after/ftplugin/asciidoc.fnl
@@ -9,18 +9,42 @@
                 :spell true
                 :spelllang :en_us})
 
-(util.set-keys [[:n
-                 :<localleader>d
-                 ":r!date '+\\%a, \\%d \\%b \\%Y' | xargs -0 printf '\\n== \\%s\\n\\n'<cr>k"
-                 {:desc "insert current date as an h2 header"
-                  :buffer (vim.api.nvim_get_current_buf)
-                  :silent true}]
-                [:n
-                 :<localleader>t
-                 ":r!date '+\\%H:\\%M' | xargs -0 printf '=== \\%s ' | tr -d '\\n'<cr>A"
-                 {:desc "insert current time as an h3 header"
-                  :buffer (vim.api.nvim_get_current_buf)
-                  :silent true}]])
+(fn insert-lines [text]
+  "Insert text at the current cursor position"
+  (let [buf (vim.api.nvim_get_current_buf)
+        (row col) (unpack (vim.api.nvim_win_get_cursor 0))
+        _row (- row 1)]
+    (vim.api.nvim_buf_set_lines buf _row (+ _row 1) false text)))
+
+(lambda insert-week []
+  (lambda find-day [dir day new-time]
+    (let [new-day (vim.fn.strftime "%a" new-time)
+          secs-in-a-day (* 60 60 24)]
+      (if (= day new-day) (vim.fn.strftime "%b %d" new-time)
+          (find-day dir day
+                    (if (= dir :fwd) (+ new-time secs-in-a-day)
+                        (- new-time secs-in-a-day))))))
+  (let [week-num (vim.fn.strftime "%U")
+        week-start (find-day :back :Mon (vim.fn.localtime))
+        week-end (find-day :fwd :Sun (vim.fn.localtime))
+        lines ["'''"
+               (.. "= Week " week-num " (" week-start " to " week-end ")")
+               ""]]
+    (insert-lines lines)))
+
+(lambda insert-day []
+  (let [curr-day (vim.fn.strftime "%a, %d %b %Y")
+        lines [(.. "== " curr-day) ""]]
+    (insert-lines lines)))
+
+(lambda insert-time []
+  (let [curr-time (vim.fn.strftime "%H:%M")
+        lines [(.. "=== " curr-time) ""]]
+    (insert-lines lines)))
+
+(lambda insert-task [] (insert-lines ["* [ ] "]))
+
+(comment (insert-week))
 
 (lambda preview-in-browser [in out browser-cmd]
   (if (util.executable? :asciidoctor)
@@ -35,3 +59,29 @@
                     #(preview-in-browser in out vim.env.BROWSER)
                     {:desc "convert to HTML and show preview in browser"
                      :buffer (vim.api.nvim_get_current_buf)})))
+
+(util.set-keys [[:n
+                 :<localleader>w
+                 insert-week
+                 {:desc "insert current week as an h2 header"
+                  :buffer (vim.api.nvim_get_current_buf)
+                  :silent true}]
+                [:n
+                 :<localleader>d
+                 ":r!date '+\\%a, \\%d \\%b \\%Y' | xargs -0 printf '\\n== \\%s\\n\\n'<cr>k"
+                 {:desc "insert current date as an h2 header"
+                  :buffer (vim.api.nvim_get_current_buf)
+                  :silent true}]
+                [:n
+                 :<localleader>t
+                 insert-time
+                 ;; ":r!date '+\\%H:\\%M' | xargs -0 printf '=== \\%s ' | tr -d '\\n'<cr>A"
+                 {:desc "insert current time as an h3 header"
+                  :buffer (vim.api.nvim_get_current_buf)
+                  :silent true}]
+                [:n
+                 :<localleader>x
+                 insert-task
+                 {:desc "insert asciidoc checkbox"
+                  :buffer (vim.api.nvim_get_current_buf)
+                  :silent true}]])
