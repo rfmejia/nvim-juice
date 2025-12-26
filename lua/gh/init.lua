ee9a57ba8899382d2c017e49ff3415bfc9bb42ef
Akie Mejia <rfmejia@juice-framework>
2025-12-25 18:33:54 -0800

    Clean up unused local modules

--- lua/gh/init.lua
+++
@@ -1,8 +0,0 @@
--- [nfnl] fnl/gh/init.fnl
-local callback
-local function _1_(output)
-  return vim.print("output", output)
-end
-callback = _1_
-local raw = vim.system({"gh", "issue", "list", "--json", "number,title,body"}, callback)
-return vim.print("raw", raw)
