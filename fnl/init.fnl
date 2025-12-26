fcb6eb6b8dfa5ea841fe1ed2d91a7e7218f67c4e
Akie Mejia <rfmejia@juice-framework>
2025-12-25 18:48:01 -0800

    Bootstrap nfnl as a pack

--- fnl/init.fnl
+++ fnl/init.fnl
@@ -1,6 +1,18 @@
-; check and install core plugins
-((-> (require :juice.bootstrap)
-     (. :setup)))
+(comment "Check and load `nfnl` pack; Clone repository if not")
+(let [nfnl-url "https://github.com/rfmejia/nfnl"
+      pack-path (.. (vim.fn.stdpath :data) :/site/pack/juice/start)
+      dir-exists? (fn [path]
+                    ((. (or vim.uv vim.loop) :fs_stat) path))]
+  (when (not (dir-exists? (.. pack-path :/nfnl)))
+    (vim.notify (string.format "[bootstrap] Cloning %s to %s..." nfnl-url
+                               pack-path))
+    (case-try (: (vim.system [:mkdir :-p pack-path]) :wait)
+      {:code 0} (: (vim.system [:git :-C pack-path :clone nfnl-url]) :wait)
+      {:code 0} (vim.notify "[bootstrap] OK")
+      (catch {: stderr}
+             (vim.notify (.. "[bootstrap] Could not clone `nfnl`: " stderr)
+                         vim.log.levels.ERROR)))
+    (vim.cmd :packloadall!)))
 
 (local {: autoload} (require :nfnl.module))
 (local util (autoload :juice.util))
--- init.lua
+++ init.lua
@@ -1,6 +1,39 @@
 -- [nfnl] fnl/init.fnl
-require("juice.bootstrap").setup()
-local _local_1_ = require("nfnl.module")
-local autoload = _local_1_.autoload
+--[[ "Check and load `nfnl` pack; Clone repository if not" ]]
+do
+  local nfnl_url = "https://github.com/rfmejia/nfnl"
+  local pack_path = (vim.fn.stdpath("data") .. "/site/pack/juice/start")
+  local dir_exists_3f
+  local function _1_(path)
+    return (vim.uv or vim.loop).fs_stat(path)
+  end
+  dir_exists_3f = _1_
+  if not dir_exists_3f((pack_path .. "/nfnl")) then
+    vim.notify(string.format("[bootstrap] Cloning %s to %s...", nfnl_url, pack_path))
+    local function _2_(...)
+      if ((_G.type(...) == "table") and ((...).code == 0)) then
+        local function _3_(...)
+          if ((_G.type(...) == "table") and ((...).code == 0)) then
+            return vim.notify("[bootstrap] OK")
+          elseif ((_G.type(...) == "table") and (nil ~= (...).stderr)) then
+            local stderr = (...).stderr
+            return vim.notify(("[bootstrap] Could not clone `nfnl`: " .. stderr), vim.log.levels.ERROR)
+          else
+            return nil
+          end
+        end
+        return _3_(vim.system({"git", "-C", pack_path, "clone", nfnl_url}):wait())
+      elseif ((_G.type(...) == "table") and (nil ~= (...).stderr)) then
+        local stderr = (...).stderr
+        return vim.notify(("[bootstrap] Could not clone `nfnl`: " .. stderr), vim.log.levels.ERROR)
+      else
+        return nil
+      end
+    end
+    _2_(vim.system({"mkdir", "-p", pack_path}):wait())
+    vim.cmd("packloadall!")
+  else
+  end
+end
 local util = autoload("juice.util")
 return util["call-setup"]("juice.options", "juice.colorscheme", "juice.plugins", "juice.mappings", "juice.commands", "juice.lsp", "juice.dotenvrc", "journal-tools", "git-info", "tmux-nav", "trim-whitespace", "wildgitignore")
