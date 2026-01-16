50a58cec7030421a05f79767bb3e70ddcf0a4492
Akie Mejia <rfmejia@pm.me>
2026-01-15 20:31:06 -0800

    Minor refactoring

--- fnl/juice/dotenvrc.fnl
+++ fnl/juice/dotenvrc.fnl
@@ -1,6 +1,6 @@
 (local {: autoload} (require :nfnl.module))
 (local core (autoload :nfnl.core))
-(local string (autoload :nfnl.string))
+(local str (autoload :nfnl.string))
 
 (comment :TODO "Load the following special envs"
   ["split to sbtn or scala-cli" :dadbod])
@@ -12,18 +12,17 @@
    :keywordprg vim.env.NVIM_KEYWORDPRG
    :formatprg vim.env.NVIM_FORMATPRG
    :wrap (= :true vim.env.NVIM_WRAP)
-   :tabstop (tonumber  vim.env.NVIM_TEXTWIDTH)
-   :textwidth (tonumber  vim.env.NVIM_TEXTWIDTH)
+   :tabstop (tonumber vim.env.NVIM_TEXTWIDTH)
+   :textwidth (tonumber vim.env.NVIM_TEXTWIDTH)
    :shiftwidth (tonumber vim.env.NVIM_SHIFTWIDTH)})
 
 (fn read-path-list []
   "Returns a list of paths from the NVIM_PATH_LIST env variable iff the env variable is not null"
   (when vim.env.NVIM_PATH_LIST
-    (core.concat ["." ""] (string.split vim.env.NVIM_PATH_LIST ":"))))
+    (core.concat ["." ""] (str.split vim.env.NVIM_PATH_LIST ":"))))
 
 (fn load-env []
-  (each [name setting (pairs (read-env-pairs))]
-    (set (. vim.opt name) setting))
+  (core.merge! vim.opt (read-env-pairs))
   (case (read-path-list)
     path-list (set vim.opt.path path-list)))
 
--- lua/juice/dotenvrc.lua
+++ lua/juice/dotenvrc.lua
@@ -2,22 +2,20 @@
 local _local_1_ = require("nfnl.module")
 local autoload = _local_1_.autoload
 local core = autoload("nfnl.core")
-local string = autoload("nfnl.string")
+local str = autoload("nfnl.string")
 --[[ "TODO" "Load the following special envs" ["split to sbtn or scala-cli" "dadbod"] ]]
 local function read_env_pairs()
   return {makeprg = vim.env.NVIM_MAKEPRG, errorformat = vim.env.NVIM_ERRORFORMAT, keywordprg = vim.env.NVIM_KEYWORDPRG, formatprg = vim.env.NVIM_FORMATPRG, wrap = ("true" == vim.env.NVIM_WRAP), tabstop = tonumber(vim.env.NVIM_TEXTWIDTH), textwidth = tonumber(vim.env.NVIM_TEXTWIDTH), shiftwidth = tonumber(vim.env.NVIM_SHIFTWIDTH)}
 end
 local function read_path_list()
   if vim.env.NVIM_PATH_LIST then
-    return core.concat({".", ""}, string.split(vim.env.NVIM_PATH_LIST, ":"))
+    return core.concat({".", ""}, str.split(vim.env.NVIM_PATH_LIST, ":"))
   else
     return nil
   end
 end
 local function load_env()
-  for name, setting in pairs(read_env_pairs()) do
-    vim.opt[name] = setting
-  end
+  core["merge!"](vim.opt, read_env_pairs())
   local case_3_ = read_path_list()
   if (nil ~= case_3_) then
     local path_list = case_3_
