cf925c56d1860ff295f9472e12334cf29e3a1247
Akie Mejia <rfmejia@juice-framework>
2026-04-02 12:11:23 -0700

    Fix PackUpdate command

--- fnl/juice/commands.fnl
+++ fnl/juice/commands.fnl
@@ -6,7 +6,7 @@
                      (util.call :juice.dotenvrc :load-env)
                      (vim.notify "Loaded environment variables"))
                   {:desc "(Re)load environment variables"}]
-        :PackUpdate [vim.pack.update {:desc "Update remote vim packages"}]})
+        :PackUpdate [#(vim.pack.update) {:desc "Update remote vim packages"}]})
 
 {:setup #(each [cmd-name args (pairs commands)]
            (vim.api.nvim_create_user_command cmd-name (unpack args)))}
--- lua/juice/commands.lua
+++ lua/juice/commands.lua
@@ -7,11 +7,14 @@ local function _1_()
   util.call("juice.dotenvrc", "load-env")
   return vim.notify("Loaded environment variables")
 end
-commands = {ClipFilename = {"let @+ = getreg('%')", {desc = "copy current file path to clipboard"}}, LoadEnv = {_1_, {desc = "(Re)load environment variables"}}, PackUpdate = {vim.pack.update, {desc = "Update remote vim packages"}}}
 local function _3_()
+  return vim.pack.update()
+end
+commands = {ClipFilename = {"let @+ = getreg('%')", {desc = "copy current file path to clipboard"}}, LoadEnv = {_1_, {desc = "(Re)load environment variables"}}, PackUpdate = {_3_, {desc = "Update remote vim packages"}}}
+local function _4_()
   for cmd_name, args in pairs(commands) do
     vim.api.nvim_create_user_command(cmd_name, unpack(args))
   end
   return nil
 end
-return {setup = _3_}
+return {setup = _4_}
