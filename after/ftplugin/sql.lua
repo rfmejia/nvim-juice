d2f42b97629ba277f1d19082845c3e088c517366
benioff <rfmejia@pm.me>
2026-09-12 17:48:53 -0700

    Set SQL stdio command for mariadb when `.my.cnf` is present

--- after/ftplugin/sql.lua
+++ after/ftplugin/sql.lua
@@ -4,16 +4,16 @@ local autoload = _local_1_.autoload
 local core = autoload("nfnl.core")
 local str = autoload("nfnl.string")
 local util = autoload("juice.util")
-vim.opt_local.commentstring = "-- %s"
 vim.g.omni_sql_default_compl_type = "syntax"
 if util["executable?"]("sqlformat") then
   --[[ "`sqlformat` is from `python-sqlparse` https://github.com/andialbrecht/sqlparse" ]]
-  local opts = {"sqlformat", "--reindent", "--keywords", "upper", "--wrap_after", 80, "-"}
-  vim.opt_local.equalprg = str.join(" ", opts)
+  local cmd = "sqlformat --reindent --keywords upper --wrap_after 80 -"
+  vim.opt_local.equalprg = cmd
 else
 end
 if vim.fn.filereadable(".my.cnf") then
-  return core["merge!"](vim.g, {["conjure#client#sql#stdio#command"] = "mariadb --defaults-file=.my.cnf"})
+  local stdio_cmd = "mariadb --defaults-file=.my.cnf --table --unbuffered"
+  return core["merge!"](vim.g, {["conjure#client#sql#stdio#command"] = stdio_cmd})
 else
   return nil
 end
--- fnl/after/ftplugin/sql.fnl
+++ fnl/after/ftplugin/sql.fnl
@@ -3,13 +3,13 @@
 (local str (autoload :nfnl.string))
 (local util (autoload :juice.util))
 
-(set vim.opt_local.commentstring "-- %s")
 (set vim.g.omni_sql_default_compl_type :syntax)
+
 (when (util.executable? :sqlformat)
   (comment "`sqlformat` is from `python-sqlparse` https://github.com/andialbrecht/sqlparse")
-  (let [opts [:sqlformat :--reindent :--keywords :upper :--wrap_after 80 "-"]]
-    (set vim.opt_local.equalprg (str.join " " opts))))
+  (let [cmd "sqlformat --reindent --keywords upper --wrap_after 80 -"]
+    (set vim.opt_local.equalprg cmd)))
 
 (if (vim.fn.filereadable :.my.cnf)
-    (core.merge! vim.g
-                 {"conjure#client#sql#stdio#command" "mariadb --defaults-file=.my.cnf"}))
+    (let [stdio-cmd "mariadb --defaults-file=.my.cnf --table --unbuffered"]
+      (core.merge! vim.g {"conjure#client#sql#stdio#command" stdio-cmd})))
